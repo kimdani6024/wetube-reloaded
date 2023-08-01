30b28ea0846658312b38f260de13b082d0ab7d3f
kimdani <kimdani6024@gmail.com>
2023-08-01 19:05:56 +0900

    SERVER

--- src/server.js
+++ src/server.js
@@ -5,12 +5,20 @@ const PORT = 4000;
 const app = express();
 
 //application 설정한 후 외부에 개방
-//브라우저가 get request를 보냄
-//get : 페이지를 가져옴
-const handleHome = () => console.log("somebody is trying to go home")
-//누군가 root page로 get request를 보낸다면 반응하는 callback
-app.get("/", handleHome); // 브라우저가 "/" url 좀 가져다줘 하는 것
 
+//express > route handler의 첫번째 argument는 request object, 두번째 argument는 response object임
+const handleHome = (req, res) => {
+    return res.send("<h1>I still love you</h1>"); 
+    // return res.end()
+}
+//누군가 root page로 get request를 보낸다면 반응하는 callback
+app.get("/", handleHome); 
+
+
+const handleLogin = (req, res) => {
+    return res.send({ message: "Login here." });
+};
+app.get("/login", handleLogin); 
 
 //listen : 서버가 시작될 때 작동하는 함수
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`)
