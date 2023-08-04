99f983fe91a72bc56ed5cf217a5fe7538e9cb337
kimdani <kimdani6024@gmail.com>
2023-08-04 14:49:20 +0900

    morgan

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const URLLogger = (req,res,next) => {
+    console.log("path: %s", req.path);
+    next()
+};
+
+const TimeLogger = (req,res,next) => {
+    const now = new Date();	// 현재 날짜 및 시간
+    const year = now.getFullYear();	// 연도
+    const month = now.getMonth(); //월
+    const date = now.getDate();	// 일
+    console.log("Time:", year,month,date);
+    // console.log('Time: %d', `${year}.${month}.${month}`);
+    next();
+};
+
+const SecurityLogger = (req,res,next) => {
+    const protocol = req.protocol;
+    // console.dir(protocol)
+    // console.log(protocol);
+    if (protocol === "https") {
+        console.log("secure")
+    }
+    else {
+        console.log("Insecure")
+    }
+    next()
+};
+
+const ProtectorLogger = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.end();
+    }
+    next();
+  };
+  
+app.use(URLLogger, TimeLogger, SecurityLogger, ProtectorLogger)
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
+
+
+// Codesandbox gives us a PORT :)
+const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`)
+app.listen(PORT, handleListening)
--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 //application 생성
 const app = express();
 
-//application 설정한 후 외부에 개방
+//logger함수는 middleware를 return해줌
+const logger = morgan("dev")
 
-//middleware
-const gossipMiddleware = (req, res, next) => {
-    //req object연습
-    console.log(`Someone is going to: ${req.url}`);
-    next();
-  };
 
-//express > route handler의 첫번째 argument는 request object, 두번째 argument는 response object임
-//finalware임으로 next argument가 필요없음
-const handleHome = (req, res) => {
-    return res.send("I love middlewares");
-    // return res.end()
-}
-//누군가 root page로 get request를 보낸다면 반응하는 callback
-//handleHome은 finalware, return시 연결이 종료되니까.
-app.get("/", gossipMiddleware, handleHome);
+//<application 설정한 후 외부에 개방>
+
+
+const home = (req,res) => {
+    console.log("I will respond");
+    return res.send("hello")
+};
+
+const login = (req,res) => {
+    return res.send("login")
+};
+
+app.use(logger) 
+app.get("/", home);
+app.get("/login", login);
 
 
 
