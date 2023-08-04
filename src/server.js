10095fd82b516c76145776b885582f09a473670e
kimdani <kimdani6024@gmail.com>
2023-08-04 19:29:35 +0900

    Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 //application 생성
@@ -12,23 +15,6 @@ app.use(logger);
 
 //<application 설정한 후 외부에 개방>
 
-
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
