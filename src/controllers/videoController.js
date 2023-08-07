a0643c33d7ead5b2e038aeaa37dc4e9fc6723130
kimdani <kimdani6024@gmail.com>
2023-08-07 19:27:29 +0900

    MVP Styles

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@
 //res.render로 home.pug를 렌더링
 //파일명은 띄어쓰기가 있으면 안됌
 //파일명은 소문자여야함
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
