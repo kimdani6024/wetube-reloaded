d4b1ba20f1f78bc4aa28a6a36303f3d9f4c34242
kimdani <kimdani6024@gmail.com>
2023-08-07 16:55:07 +0900

    Partials

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import userRouter from "./routers/userRouter";
 const PORT = 4000;
 
 //현재 작업 디렉토리
-console.log(process.cwd())
+// console.log(process.cwd())
 
 //application 생성
 const app = express();
@@ -21,6 +21,7 @@ const logger = morgan("dev")
 
 //express가 views 디렉토리에서 pug파일을 찾도록 설정
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
