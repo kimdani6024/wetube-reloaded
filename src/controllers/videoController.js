939c99e1c6a8ef23322cda5c72f64c476361cebc
kimdani <kimdani6024@gmail.com>
2023-08-09 16:38:51 +0900

    Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,14 @@
+const fakeUser = {
+  username: "Kimdani",
+  loggedIn: true,
+};
+
 //view name 쓰기 home
 //res.render로 home.pug를 렌더링
 //파일명은 띄어쓰기가 있으면 안됌
 //파일명은 소문자여야함
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
