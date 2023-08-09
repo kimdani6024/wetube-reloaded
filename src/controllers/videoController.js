ab15d4801864d92f5af8160015e39307aa7a68c1
kimdani <kimdani6024@gmail.com>
2023-08-09 17:04:07 +0900

    Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,23 @@
-const fakeUser = {
-  username: "Kimdani",
-  loggedIn: true,
-};
 
 //view name 쓰기 home
 //res.render로 home.pug를 렌더링
 //파일명은 띄어쓰기가 있으면 안됌
 //파일명은 소문자여야함
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
-
+export const trending = (req, res) => {
+  // 배열이 없으면 동작하지 않음
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 
