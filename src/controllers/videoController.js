9acab47ea698b7428cf6154a658269110b90acd8
kimdani <kimdani6024@gmail.com>
2023-08-09 19:57:43 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,8 +41,13 @@ export const watch = (req, res) => {
   // watch라는 템플릿을 render해줌
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
 
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  //수정할 비디오 id를 찾아야함
+  const { id } = req.params;
+  const video = videos[id - 1];
+  
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {};
