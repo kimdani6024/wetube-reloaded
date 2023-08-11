94276819f55ce6c2f836be6d5b765eb8bb03aa76
kimdani <kimdani6024@gmail.com>
2023-08-11 20:01:53 +0900

    Video Model

--- src/models/video.js
+++ src/models/video.js
@@ -1,6 +1,7 @@
-//mongoose에게 우리 애플리케이션의 데이터들이 어떻게 생겼는지 알려줘야함
 import mongoose from "mongoose";
 
+//mongoose에게 우리 애플리케이션의 데이터들이 어떻게 생겼는지 알려줘야함
+//model의 형태를 정의해줌.  = schema라고 부름
 const videoSchema = new mongoose.Schema({
   title: String,
   description: String,
@@ -12,5 +13,6 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+// model이름+ 데이터형태인 schema로 구성
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
