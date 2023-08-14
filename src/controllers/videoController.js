c663234aacdfee1b15e7dd9a86e4b137eda7a269
kimdani <kimdani6024@gmail.com>
2023-08-14 14:19:25 +0900

    Returns and Renders

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,18 +5,33 @@ import Video from "../models/Video";
 //res.render로 home.pug를 렌더링
 //파일명은 띄어쓰기가 있으면 안됌
 //파일명은 소문자여야함
-export const home = (req, res) => {
-  //database와 연결
-  //{}가 비어있으면 모든 형식을 찾는다는 걸 뜻함. 모든 형태의 비디오를 찾는 것 
-  //그다음단계로 callback을 전송 : err, docs라는 signature를 가짐 docs는 videos로 바꿔도 상관없음
-  console.log("Start");
-  //moongoose는 {}부분을 가져오고 ()부분을 실행시킴
-  //database검색이 안 끝났을 때 render되는 걸 방지하기 위함
-  Video.find({}, (error, videos) => {
-    console.log("Finished");
-    return res.render("home", { pageTitle: "Home", videos });
-  });
-  console.log("I finish first");
+
+
+// export const home = (req, res) => {
+//   //database와 연결
+//   //{}가 비어있으면 모든 형식을 찾는다는 걸 뜻함. 모든 형태의 비디오를 찾는 것 
+//   //그다음단계로 callback을 전송 : err, docs라는 signature를 가짐 docs는 videos로 바꿔도 상관없음
+//   console.log("Start");
+//   //moongoose는 {}부분을 가져오고 ()부분을 실행시킴
+//   //database검색이 안 끝났을 때 render되는 걸 방지하기 위함
+//   //callback의 장점은 추가적인 코드없이 에러들을 바로 볼 수 있음
+//   //start, finished 다음에 videos를 출력
+//   Video.find({}, (error, videos) => {
+//   if(error){
+//     return res.render("server-error")
+//   }
+//   console.log("I finish first");
+// };
+export const home = async (req, res) => {
+  try {
+  //위에서 아래로 순서대로 출력해줌 
+  //코드 실행 중 오류 발생시 아래코드 출력해줌 
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+}
+  catch {
+    return res.render("server-error")
+  }
 };
 
 export const watch = (req, res) => {
